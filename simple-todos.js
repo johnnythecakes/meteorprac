50e749d7c39b4adeb4cad55c9110b68e57aac67a
johnnythecakes <1zachjohnson@gmail.com>
2014-11-23 11:17:14 -0800

    added update and delete to task template

--- simple-todos.js
+++ simple-todos.js
@@ -5,7 +5,7 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
     // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
@@ -25,6 +25,16 @@ if (Meteor.isClient) {
 
       // Prevent default form submit
       return false;
+    }
+  });
+    // In the client code, below everything else
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
     }
   });
 }
