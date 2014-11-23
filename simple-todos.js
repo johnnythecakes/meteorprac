5a59b2257bb4ace34be344c3f7346514196365bc
johnnythecakes <1zachjohnson@gmail.com>
2014-11-23 12:12:20 -0800

    added user login

--- simple-todos.js
+++ simple-todos.js
@@ -3,9 +3,23 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // This code only runs on the client
+  // Replace the existing Template.body.helpers
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+        // Add to Template.body.helpers
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
     // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
@@ -17,7 +31,9 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
       });
 
       // Clear form
@@ -25,6 +41,10 @@ if (Meteor.isClient) {
 
       // Prevent default form submit
       return false;
+    },
+        // Add to Template.body.events
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
     // In the client code, below everything else
@@ -36,5 +56,9 @@ if (Meteor.isClient) {
     "click .delete": function () {
       Tasks.remove(this._id);
     }
+  });
+    // At the bottom of the client code
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
